c616b6c9390034dea2f82fb138c629b88c397471
苑成文 <yuanchegnwen@gmail.com>
2020-07-03 15:00:22 +0800

    updateVersion

--- upload.sh
+++ upload.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-VersionNumber=`grep -E 'spec.version.*=' CWCrashProtect.podspec`
+VersionString=`grep -E 'spec.version.*=' CWCrashProtect.podspec`
+VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
 git add .
 git commit -m 'updateVersion'
 git push
 echo "New Version ${VersionNumber}"
 
-git tag "${VersionNumber}"
+git tag ${VersionNumber}
 git push origin master --tags
 pod trunk push ./CWCrashProtect.podspec --verbose --allow-warnings
