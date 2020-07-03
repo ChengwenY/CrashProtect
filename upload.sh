b2f237b3af62f80bc025e008ccbe644e9b596669
苑成文 <yuanchegnwen@gmail.com>
2020-07-03 15:43:16 +0800

    updateVersion

--- upload.sh
+++ upload.sh
@@ -2,7 +2,9 @@
 
 VersionString=`grep -E 'spec.version.*=' CWCrashProtect.podspec`
 VersionArr=(`echo $VersionString | tr '=' ' '`)
-VersionNumber=${${VersionArr[2]}//'"'/''}
+VersionNumberStr=${VersionArr[2]}
+VersionNumber=(${VersionNumberStr}//'"'/'')
+
 git add .
 git commit -m 'updateVersion'
 git push
